11cee62f6055a8e9a8840aab761182a5c2730ef7
Yoshua Wuyts <yoshuawuyts@gmail.com>
2014-01-12 17:11:14 +0100

    finish 8

---
+++ spec/requests/authentication_pages_spec.rb
@@ -0,0 +1,32 @@
+require 'spec_helper'
+
+describe "Authentication" do
+
+  subject { page }
+
+  describe "signin page" do
+    before { visit signin_path }
+
+    it { should have_content('Sign in') }
+    it { should have_title('Sign in') }
+  end
+
+describe "signin" do
+
+    before { visit signin_path }
+
+    describe "with invalid information" do
+      before { click_button "Sign in" }
+
+      it { should have_title('Sign in') }
+      it { should have_selector('div.alert.alert-error') }
+
+      describe "after visiting another page" do
+        before { click_link "Home" }
+        it { should_not have_selector('div.alert.alert-error') }
+      end
+
+
+    end
+  end
+end
