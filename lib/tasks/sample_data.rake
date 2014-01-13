55ba743c717e7f38bea100694f416ba36ea199b7
Yoshua Wuyts <yoshuawuyts@gmail.com>
2014-01-13 18:19:57 +0100

    9.2

--- lib/tasks/sample_data.rake
+++ lib/tasks/sample_data.rake
@@ -6,8 +6,10 @@ namespace :db do
                          password: "foobar",
                          password_confirmation: "foobar",
                          admin: true)
-    .
-    .
-    .
+    users = User.all(limit: 6)
+    50.times do
+      content = Faker::Lorem.sentence(5)
+      users.each { |user| user.microposts.create!(content: content) }
+    end
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,6 +21,9 @@ describe User do
   it { should be_valid }
   it { should_not be_admin }
 
+  it { should respond_to(:admin) }
+  it { should respond_to(:microposts) }
+
   describe "with admin attribute set to 'true'" do
     before do
       @user.save!
@@ -109,5 +112,29 @@ describe User do
   describe "remember token" do
     before { @user.save }
     its(:remember_token) { should_not be_blank }
+  end
+
+  describe "micropost associations" do
+
+    before { @user.save }
+    let!(:older_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+    end
+    let!(:newer_micropost) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+    end
+
+    it "should have the right microposts in the right order" do
+      expect(@user.microposts.to_a).to eq [newer_micropost, older_micropost]
+    end
+    
+    it "should destroy associated microposts" do
+      microposts = @user.microposts.to_a
+      @user.destroy
+      expect(microposts).not_to be_empty
+      microposts.each do |micropost|
+        expect(Micropost.where(id: micropost.id)).to be_empty
+      end
+    end
   end
 end
